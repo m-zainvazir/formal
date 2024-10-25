560fa469a0235315a5eeafb7afc93c1162bf419b
Muhammad Zain Vazir <zainvazir1@gmail.com>
2024-10-25 23:04:55 +0100

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-hyphen_edot = '_e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    Through this func, the list of requirements will be returned
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
-        requirements= file_obj.readlines()
-        requirements= [req.replace("\n","") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if hyphen_edot in requirements:
-            requirements.remove(hyphen_edot)
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
